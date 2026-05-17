e03f40d95213fb7c1bb39d14e4c190a78d619920
Harini Padavala <harinipadavala@gmail.com>
2026-05-17 20:30:38 +0200

    Fix get_user_by_email method

--- backend/src/controllers/usercontroller.py
+++ backend/src/controllers/usercontroller.py
@@ -2,29 +2,26 @@ from src.controllers.controller import Controller
 from src.util.dao import DAO
 
 import re
-
 emailValidator = re.compile(r'.*@.*')
 
-
 class UserController(Controller):
-
     def __init__(self, dao: DAO):
         super().__init__(dao=dao)
 
     def get_user_by_email(self, email: str):
-        """
-        Given a valid email address of an existing account,
-        return the user object associated with that email.
-
+        """Given a valid email address of an existing account, return the user object contained in the database associated 
+        to that user. For now, do not assume that the email attribute is unique. Additionally print a warning message containing the email
+        address if the search returns multiple users.
+        
         parameters:
-            email -- an email address string
+            email -- an email address string 
 
         returns:
-            user -- the user object associated to that email address
+            user -- the user object associated to that email address (if multiple users are associated to that email: return the first one)
             None -- if no user is associated to that email address
 
         raises:
-            ValueError -- in case the email parameter is not valid
+            ValueError -- in case the email parameter is not valid (i.e., conforming <local-part>@<domain>.<host>)
             Exception -- in case any database operation fails
         """
 
@@ -32,30 +29,21 @@ class UserController(Controller):
             raise ValueError('Error: invalid email address')
 
         try:
-
             users = self.dao.find({'email': email})
-
             if len(users) == 0:
                 return None
             
             if len(users) > 1:
                 print(f'Warning: more than one user found with email {email}')
-            
-            return users[0]
 
+            return users[0]
+                    
         except Exception as e:
-            raise e
+            raise
 
     def update(self, id, data):
-
         try:
-
-            update_result = super().update(
-                id=id,
-                data={'$set': data}
-            )
-
+            update_result = super().update(id=id, data={'$set': data})
             return update_result
-
         except Exception as e:
-            raise e
+            raise
