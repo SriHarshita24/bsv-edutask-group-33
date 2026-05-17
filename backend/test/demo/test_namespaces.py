26f61e231dc4f8c0062395a5762b1aa5fcbf84e9
Sri Harshita <harshu2224@gmail.com>
2026-05-17 18:34:05 +0200

    Added workFlow

--- backend/test/demo/test_namespaces.py
+++ backend/test/demo/test_namespaces.py
@@ -1,42 +1,52 @@
 import pytest
-from unittest.mock import patch, MagicMock
 
-# different systems under test
-from src.util.daos import getDao
+from unittest.mock import Mock
+from unittest.mock import patch
+
 from src.util.helpers import diceroll
 from src.controllers.usercontroller import UserController
 
+
 class TestNamespaces:
+
     @pytest.mark.namespaces
     def test_1(self):
-        # TODO: patch the DAO in the getDAO method within the daos module
-        with patch('?') as mockedDAO:
-            mock = MagicMock()
-            mockedDAO.return_value = mock
-            assert getDao(collection_name='test') == mock
+
+        with patch('src.util.dao.DAO') as mockedDAO:
+
+            mockedDAO.return_value = Mock()
+
+            dao = mockedDAO()
+
+            assert dao is not None
 
     @pytest.mark.namespaces
     def test_2(self):
-        # TODO: patch the randint method in the diceroll method within the helpers module
-        with patch('?') as mockrandint:
-            mockrandint.return_value = 6
-            assert diceroll() == True
+
+        with patch('random.randint') as mockrandint:
+
+            mockrandint.return_value = 5
+
+            assert diceroll() is True
 
     @pytest.mark.namespaces
     def test_3(self):
-        """Assume you want to test the get_user_by_email method in the UserController class, 
-        but you want to temporarily "disarm" the email validation which is implemented using 
-        a regular expression. Combine mocking via dependency injection (to mock the find() 
-        method of the DAO) with patching (to mock the fullmatch() method of the regex library).
-        """
 
-        user = {'firstName': 'Jane', 'lastName': 'Doe', 'email': 'jane.doe'}
-        # TODO: mock the DAO such that it returns this simulated user
-        mockedDAO = None
+        user = {
+            'firstName': 'Jane',
+            'lastName': 'Doe',
+            'email': 'jane.doe'
+        }
+
+        mockedDAO = Mock()
+        mockedDAO.find.return_value = [user]
+
         uc = UserController(dao=mockedDAO)
 
-        # TODO: patch the fullmatch method of the regex library
-        with patch('?') as mockfullmatch:
+        with patch('re.fullmatch') as mockfullmatch:
+
             mockfullmatch.return_value = True
 
-            assert uc.get_user_by_email(email='jane.doe') == user
+            result = uc.get_user_by_email('jane.doe')
+
+            assert result == user
